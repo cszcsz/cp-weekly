713c782351fa580230b5dc38d4c5f55273df2612
caisongzhi <175837951@qq.com>
2017-11-03 23:51:41 +0800

    增加爱心上下变速滑动效果

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,13 @@ $('a').click(function () {
         move();
 })
 function move(){
-    $("#xin2").animate({top:'30px'},"slow",function(){
-      $("img").fadeOut("slow");
-      setTimeout("window.location='next.html'",1000)
+    $("#xin2").animate({top:'48px'},2000,"easeOutCirc",function(){
+
     });
+  $("#xin1").animate({top:'25px'},2000,"easeOutCirc",function(){
+    $("img").fadeOut("slow");
+    setTimeout("window.location='next.html'",1000)
+  });
 }
 
 
