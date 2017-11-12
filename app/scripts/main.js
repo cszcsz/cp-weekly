32e6042e9303ae2033ef4f6eac3b33fe8b17ccfd
caisongzhi <175837951@qq.com>
2017-11-12 12:32:29 +0800

    样式大体完成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,10 @@ $('a').click(function () {
         move();
 })
 function move(){
-    $("#xin2").animate({top:'48px'},2000,"easeOutCirc",function(){
+    $("#xin2").animate({top:'46px'},1500,"easeOutCirc",function(){
 
     });
-  $("#xin1").animate({top:'25px'},2000,"easeOutCirc",function(){
+  $("#xin1").animate({top:'33px'},1500,"easeOutCirc",function(){
     $("img").fadeOut("slow");
     setTimeout("window.location='next.html'",1000)
   });
